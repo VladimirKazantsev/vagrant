ea087879b3ed972fce3004b55003acc94de78833
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2024-10-22 15:27:41 +0300

    Добавил песочницу

---
+++ sandbox/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y net-tools
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker $USER
+docker volume create portainer_data
+docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+
+sudo cp -r /tmp/vault_jenkins/ /root/
+cd /root/vault_jenkins/
+sudo docker compose up -d
