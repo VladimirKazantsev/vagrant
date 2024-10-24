9aa9feaeb300889faf56184cf4bec2e15327ea0f
Казанцев Владимир Сергеевич <Vl.Kazantsev@ase-ec.ru>
2024-10-24 15:17:30 +0300

    ~

--- sandbox/vault_jenkins/init_vault.sh
+++ sandbox/vault_jenkins/init_vault.sh
@@ -4,8 +4,8 @@
 INIT_OUTPUT=$(vault operator init -key-shares=1 -key-threshold=1)
 
 # Извлечение ключа и токена из вывода
-UNSEAL_KEY=$(echo "$INIT_OUTPUT" | grep 'Unseal Key 1:' | awk '{print $NF}')
-ROOT_TOKEN=$(echo "$INIT_OUTPUT" | grep 'Initial Root Token:' | awk '{print $NF}')
+UNSEAL_KEY=$(echo "$INIT_OUTPUT" | grep "Unseal Key 1:" | awk "{print $NF}")
+ROOT_TOKEN=$(echo "$INIT_OUTPUT" | grep "Initial Root Token:" | awk "{print $NF}")
 
 # Размораживание Vault
 vault operator unseal "$UNSEAL_KEY"
